3f55e3f615a3043afdd1751a06b6507bba5deb45
gallardo747 <132841308+gallardo747@users.noreply.github.com>
2023-11-20 13:05:05 -0300

    Commit 1.3
    Commit 1.3

--- front_vetly-main/vite.config.js
+++ front_vetly-main/vite.config.js
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    // Ajusta la configuración de la salida (output) según sea necesario
+    outDir: 'build',
+    // Puedes ajustar la ruta del servidor si es necesario
+    assetsDir: 'assets',
+    chunkSizeWarningLimit: 5000,
+  },
+});
