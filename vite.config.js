a265f274a8490f86c62f43b65f11c59c3d5c23f2
gallardo747 <132841308+gallardo747@users.noreply.github.com>
2023-11-20 17:05:07 -0300

    Commit 1.9
    Commit 1.9

--- vite.config.js
+++ vite.config.js
@@ -6,19 +6,26 @@ import path from 'path';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  //DEBO MEJORAR ÉSTE ALIAS porque no me muestra las imagenes
-  //-----------------------------------------------------------------------
   resolve: {
     alias: {
       '/@': path.resolve(__dirname, 'src'), // Cambié '/src' a '/@' para evitar conflictos
     },
   },
-  //----------------------------------------------------------------------
   build: {
-    // Ajusta la configuración de la salida (output) según sea necesario
     outDir: 'build',
-    // Puedes ajustar la ruta del servidor si es necesario
     assetsDir: 'assets',
     chunkSizeWarningLimit: 5000,
+  },
+  rollupOptions: {
+    // Agrega la configuración de resolución de alias para Rollup
+    plugins: [
+      {
+        name: 'vite-plugin-alias',
+        configure(aliases) {
+          // Usa el mismo alias que en la configuración de Vite para Rollup
+          aliases['/@'] = path.resolve(__dirname, 'src');
+        },
+      },
+    ],
   },
 });
